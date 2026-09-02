64078d48bc002cd01c65bc5e33c5e58a5e7293c0
masuda2024 <k024g0042@g.neec.ac.jp>
2026-09-02 10:05:01 +0900

    チーム番号と暫定タイトルを追加

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -52,7 +52,7 @@ Over* over = nullptr;
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 {
 	// エンジンの初期化
-	KamataEngine::Initialize(L"_");
+	KamataEngine::Initialize(L"3068_注意落とし穴!");
 
 	// DirectXCommonインスタンスを取得する
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
